1280dc179cc84dfe63fac44f26a744d772c092aa
unknown <Python@BUE302D04.alunos.sc.senac.br>
2021-02-02 17:19:16 -0300

    Alteração Para Forms na HomePage.  Atualização da 'principia'

--- financr/faleconosco/models.py
+++ financr/faleconosco/models.py
@@ -4,3 +4,6 @@ class Faleconosco(models.Model):
     nome = models.CharField(max_length = 255)
     email = models.EmailField(max_length = 255)
     mensagem = models.CharField(max_length = 4000)
+
+    def __str__(self):
+        return self.nome
---
+++ financr/faleconosco/static/financr/arquivos_js/js_da_home_do_webapp/script.js
@@ -0,0 +1,12 @@
+mostrar = true
+
+const opcoes_icone_usuario = document.querySelector('.opcoes_icone_usuario')
+const icone_usuario = document.querySelector('#icone_usuario')
+
+icone_usuario.addEventListener('click', () => {
+
+    opcoes_icone_usuario.classList.toggle('on', mostrar)
+
+    mostrar = !mostrar
+
+})
