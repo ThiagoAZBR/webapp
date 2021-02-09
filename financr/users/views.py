7fbe954aed61e12b13a4907c597b9a4c0df52cf6
unknown <Python@BUE302D04.alunos.sc.senac.br>
2021-02-09 13:33:10 -0300

    Ver se Funciona

--- financr/users/views.py
+++ financr/users/views.py
@@ -41,3 +41,16 @@ def register(request):
             return redirect(reverse("dashboard"))
         else:
             return render(request, "users/error.html", {'form':form})
+
+from django.contrib.auth import authenticate, login
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        redirect()
+    else:
+        print('Deu Erro Novax')
