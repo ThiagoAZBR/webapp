3103b7867b7799f1d38e88830ae41f5ab7d963a1
ThiagoAZ <thiagoaugustozeferino@gmail.com>
2021-02-14 20:56:57 -0300

    Commit Para Poder Usar

--- financr/faleconosco/static/financr/arquivos_js/js_transacoes/roteiro3.js
+++ financr/faleconosco/static/financr/arquivos_js/js_transacoes/roteiro3.js
@@ -1,18 +1,21 @@
 let check1 = document.querySelector('#check1')
 let div_check1 = document.querySelector('.div_check1')
+// Apenas Observação irá aparecer
+let observaca = document.querySelector('.div_observacao')
 
 let check2 = document.querySelector('#check2')
 let div_check2 = document.querySelector('.div_check2')
+// Observação e Regularidade Irão Aparecer
+let regularide = document.querySelector('.div_regularidade')
 
 let check3 = document.querySelector('#check3')
 let div_check3 = document.querySelector('.div_check3')
+// Observação, Regularidade e Parcelas irão Aparecer
+let parcelas = document.querySelector('.div_numero_de_parcelas')
 
-let div_pontual = document.querySelector('.div_tipo_pontual')
-let div_tipo_fixa = document.querySelector('.div_tipo_fixa')
-let div_tipo_parcelada = document.querySelector('.div_tipo_parcelada')
+let div_forms = document.querySelector('.div_tipo_parcelada')
 
 let resetar = document.querySelector('#resetar')
-
 
 resetar.addEventListener('click', function() {
 
@@ -20,9 +23,7 @@ resetar.addEventListener('click', function() {
     div_check2.style.display = 'flex'
     div_check3.style.display = 'flex'
 
-    div_pontual.style.display = 'none'
-    div_tipo_fixa.style.display = 'none'
-    div_tipo_parcelada.style.display = 'none'
+    div_forms.style.display = 'none'
 
 
 
@@ -32,15 +33,17 @@ check1.onchange = function () {
 
     if (check1.checked) {
 
-        div_pontual.style.display = 'block'
+        div_forms.style.display = 'block'
         div_check2.style.display = 'none'
         div_check3.style.display = 'none'
+        regularide.style.display = 'none'
+        parcelas.style.display = 'none'
 
     }
 
     else {
 
-        div_pontual.style.display = 'none'
+        div_forms.style.display = 'none'
         div_check2.style.display = 'flex'
         div_check3.style.display = 'flex'
 
@@ -52,15 +55,17 @@ check2.onchange = function () {
 
     if (check2.checked) {
 
-        div_tipo_fixa.style.display = 'block'
+        div_forms.style.display = 'block'
         div_check1.style.display = 'none'
         div_check3.style.display = 'none'
+        regularide.style.display = 'block'
+        parcelas.style.display = 'none'
 
     }
 
     else {
 
-        div_tipo_fixa.style.display = 'none'
+        div_forms.style.display = 'none'
         div_check1.style.display = 'flex'
         div_check3.style.display = 'flex'
 
@@ -72,16 +77,18 @@ check3.onchange = function () {
 
     if (check3.checked) {
 
-        div_tipo_parcelada.style.display = 'block'
+        div_forms.style.display = 'block'
         div_check1.style.display = 'none'
         div_check2.style.display = 'none'
+        regularide.style.display = 'block'
+        parcelas.style.display = 'block'
         console.log('Check3 Aqui')
 
     }
 
     else {
 
-        div_tipo_parcelada.style.display = 'none'
+        div_forms.style.display = 'none'
         div_check1.style.display = 'flex'
         div_check2.style.display = 'flex'
         console.log('Check3 Falso Aqui')
