e73f37e809665673552307eaf500013c3c1c699c
unknown <Python@BUE302D05.alunos.sc.senac.br>
2021-02-17 17:18:24 -0300

    Funcionando A Transição de Imagens

--- financr/faleconosco/static/financr/arquivos_js/js_da_home_do_webapp/grafico.js
+++ financr/faleconosco/static/financr/arquivos_js/js_da_home_do_webapp/grafico.js
@@ -19,3 +19,78 @@
 //     const chart = new google.visualization.PieChart(container)
 //     chart.draw(data, options)
 // }
+
+let circulo1 = document.querySelector('#circulo_1')
+let circulo2 = document.querySelector('#circulo_2')
+let circulo3 = document.querySelector('#circulo_3')
+
+let grafico_pizza     =  document.querySelector('#graficopizza')
+let grafico_despesas  =  document.querySelector('#graficodespesas')
+let grafico_receitas  =  document.querySelector('#graficoreceitas')
+
+let txt_pizza   = document.querySelector('#txt_pizza')
+let txt_despesa = document.querySelector('#txt_despesa')
+let txt_receita = document.querySelector('#txt_receita')
+
+let lista = [grafico_despesas, grafico_pizza, grafico_receitas]
+let lista2 = [txt_pizza, txt_receita, txt_despesa]
+
+let max = lista.length
+
+circulo1.addEventListener('click', function() {
+
+
+    for (let i = 0;; i++) {
+
+        if (i >= max) break;
+
+        lista[i].style.display = 'none'
+        lista2[i].style.display = 'none'
+
+    }
+
+    grafico_pizza.style.display = 'flex'
+    txt_pizza.style.display = 'block'
+
+})
+
+
+circulo2.addEventListener('click', function() {
+
+
+    for (let i = 0;; i++) {
+
+        if (i >= max) break;
+
+        lista[i].style.display = 'none'
+        lista2[i].style.display = 'none'
+    
+    }
+
+    grafico_despesas.style.display = 'flex'
+    txt_despesa.style.display = 'block'
+
+})
+
+
+circulo3.addEventListener('click', function() {
+
+
+    for (let i = 0;; i++) {
+
+        if (i >= max) break;
+
+
+        lista[i].style.display = 'none'
+        lista2[i].style.display = 'none'
+
+
+    
+    }
+
+    grafico_receitas.style.display = 'flex'
+    txt_receita.style.display = 'block'
+
+})
+
+
