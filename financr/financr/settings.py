42fc202ddca042f133b32ee567b1e1c4183e3b74
helionroloff <helionroloff@gmail.com>
2021-02-22 16:07:07 -0300

    teste heroku

--- financr/financr/settings.py
+++ financr/financr/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from .settings import django_heroku
 
 from pathlib import Path
 
@@ -136,4 +135,3 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
 
-django_heroku.settings(locals())
